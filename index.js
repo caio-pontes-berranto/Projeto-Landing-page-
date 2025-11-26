9a10d41218eb454273dfe37e77d66c7e2ea8262a
caio pontes <caio.pontes.escola@gmail.com>
2025-11-26 12:30:08 -0300

    add projetos

---
+++ index.js
@@ -0,0 +1,22 @@
+
+var leonardo = window.document.GetElementById("Leonardo")
+var bruna = window.document.GetElementById("Bruna")
+var samanta = window.document.GetElementById("Samanta")
+
+var setadireita = window.document.GetElementById("SetaDireita")
+var setaesquerda = window.document.GetElementById("SetaEsquerda")
+
+
+function RolarParaDireita() {
+   Leonardo.style="display:none"
+   Bruna.style="display:flex"
+   SetaDireita.style="display:none"
+   SetaEsquerda.style="display:flex"
+}
+
+function RolarParaEsquerda() {
+   Leonardo.style="display:flex"
+   Bruna.style="display:none"
+   SetaDireita.style="display:flex"
+   SetaEsquerda.style="display:none"
+}
